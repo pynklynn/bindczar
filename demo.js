fba1a0c9d739b2b5d7fb455be5c5b9b781e49196
Will Steinmetz <willsteinmetz@me.com>
2021-02-27 01:51:13 -0500

    Working on initializing values

--- demo.js
+++ demo.js
@@ -7,18 +7,62 @@ const prop1 = {
   attribute: 'person-name'
 };
 
+const prop2 = {
+  type: Number,
+  name: 'num',
+  // value: 100
+};
+
+const prop3 = {
+  type: String,
+  name: 'textArea',
+  attribute: 'text-area'
+};
+
+const prop4 = {
+  type: String,
+  name: 'dropDown',
+  attribute: 'drop-down'
+};
+
 const TestComponent = BindCzar(class extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <p>
         Hello, <span bc-name>${this.name}</span>!
       </p>
+      <p>
+        <input type="text" bc-name value="${this.name}">
+      </p>
+      <tr>
+      <p>
+        I'm a number: <span bc-num>${this.num}</span>!
+      </p>
+      <p>
+        <input type="number" bc-num value="${this.num}">
+      </p>
+      <p bc-textArea></p>
+      <div>
+        <textarea bc-textArea>${this.textArea}</textArea>
+      </div>
+      <p bc-dropDown></p>
+      <p>
+        <select bc-dropDown>
+          <option value="one">one</option>
+          <option value="two">two</option>
+          <option value="three">three</option>
+          <option value="four">four</option>
+          <option value="five">five</option>
+        </select>
+      </p>
     `;
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
-    console.log('bam!!!!!!');
+    console.log('bam!!!');
+    // console.log(name);
+    // console.log(newValue);
   }
-}, prop1);
+}, prop1, prop2, prop3, prop4);
 
 export { TestComponent }
--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,8 @@ function BindCzar(o: any, ...props: BCPropertySpec[]) {
     o._meta = {};
   }
 
+  o.prototype._initializedProperties = [];
+
   for (const prop of props) {
     const elementAttr = prop.attribute ? prop.attribute : prop.name;
 
@@ -25,30 +27,96 @@ function BindCzar(o: any, ...props: BCPropertySpec[]) {
         name: prop.name,
         type: prop.type
       };
+      if (prop.value) {
+        o._meta[elementAttr].default = prop.value;
+      }
     }
 
     Object.defineProperty(o.prototype, prop.name, {
       get() {
-        return this.getAttribute(elementAttr);
+        // TODO handle re-typing
+        // return this.getAttribute(elementAttr);
+        return this[`_${prop.name}`];
       },
       set(newValue: any) {
         try {
-          this.setAttribute.apply(this, [elementAttr, newValue.toString()]);
+          this[`_${prop.name}`] = newValue;
+          // TODO handle complex types
+          if (newValue.toString() != this.getAttribute(elementAttr)) {
+            // TODO handle appropriate stringifying (eg arrays and objects)
+            this.setAttribute.apply(this, [elementAttr, newValue.toString()]);
+          }
+          Array.from(this.querySelectorAll([`[bc-${prop.name.toLowerCase()}]`])).forEach((el): void => {
+            if ((el as HTMLSelectElement).querySelectorAll('option').length) {
+              const selectBox = el as HTMLSelectElement;
+              const selectedOption = selectBox.querySelector('option[selected]');
+              if (selectedOption) {
+                selectedOption.removeAttribute('selected');
+              }
+              const newSelectedOption = selectBox.querySelector(`option[value="${newValue.toString()}"]`);
+              if (newSelectedOption) {
+                newSelectedOption.setAttribute('selected', '');
+              }
+            } else if ((el as HTMLInputElement).value) {
+              (el as HTMLInputElement).value = newValue.toString();
+            } else {
+              (el as HTMLElement).innerHTML = newValue.toString();
+            }
+            // TODO handle select
+          });
         } catch(e: unknown) {
           // do nothing - often will fail during attachment but work after
+          // console.log(e);
         }
       }
     });
+    // TODO default value not being set when the attribute isn't available
+    /// possibly in connectedCallback?
     // TODO make work right for arrays and objects
-    o.prototype[prop.name] = prop.value ? new prop.type(prop.value) : new prop.type();
+    // o.prototype[prop.name] = prop.value ? new prop.type(prop.value) : new prop.type();
   }
 
   const existingAttributeChangedCallback = o.prototype.attributeChangedCallback;
-  o.prototype.attributeChangedCallback = (name: string, oldValue: string, newValue: string) => {
-    if (existingAttributeChangedCallback) {
-      existingAttributeChangedCallback(name, oldValue, newValue);
+  Object.defineProperty(o.prototype, 'attributeChangedCallback', {
+    value(name: string, oldValue: string, newValue: string) {
+      if (existingAttributeChangedCallback) {
+        existingAttributeChangedCallback.apply(this, [name, oldValue, newValue]);
+      }
+      if (o._meta.hasOwnProperty(name)) {
+        const meta = o._meta[name];
+        // TODO handle complex types
+        o.prototype[meta.name] = new meta.type(newValue);
+      }
     }
-  }
+  });
+
+  const existingConnectedCallback = o.prototype.connectedCallback;
+  Object.defineProperty(o.prototype, 'connectedCallback', {
+    value() {
+      if (existingConnectedCallback) {
+        existingConnectedCallback.apply(this);
+      }
+
+      const attributes = Object.keys(o._meta);
+      for (let i = 0, len = attributes.length; i < len; i++) {
+        const meta = o._meta[attributes[i]];
+        const attribute = meta.name;
+        // this[meta.name] = meta.default ? new meta.type(meta.default) : new meta.type();
+        this[meta.name] = meta.default ? new meta.type(meta.default) : null;
+        // if (meta.default) {
+        //   this[meta.name] = new meta.type(meta.default);
+        // }
+        const selector = `textarea[bc-${attribute}],input[bc-${attribute}],select[bc-${attribute}]`;
+        Array.from(document.querySelectorAll(selector)).forEach(el => {
+          (el as HTMLElement).addEventListener('input', (event: Event) => {
+            // TODO handle complex types
+            const target = (event.target as HTMLInputElement);
+            this[attribute] = new meta.type(target.value);
+          });
+        });
+      }
+    }
+  });
 
   return o;
 }
